bdbeda4d384564120b1c60dc307c118b1aa2a6c9
Unknown <danila.vpopov@gmail.com>
2022-03-22 21:09:23 -0700

    this mess is confusing

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,18 +1,49 @@
 const router = require('express').Router();
-const bcrypt = require('bcrypt');
-const User = require('../../models/User');
+const { User } = require('../../models');
 
-// CREATE a new user
-router.post('/', async (req, res) => {
+router.post('/login', async (req, res) => {
   try {
-    const newUser = req.body;
-    // hash the password from 'req.body' and save to newUser
-    newUser.password = await bcrypt.hash(req.body.password, 10);
-    // create the newUser with the hashed password and save to DB
-    const userData = await User.create(newUser);
-    res.status(200).json(userData);
+    // TODO: Add a comment describing the functionality of this expression
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // TODO: Add a comment describing the functionality of this expression
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // TODO: Add a comment describing the functionality of this method
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    // TODO: Add a comment describing the functionality of this method
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
   }
 });
 
