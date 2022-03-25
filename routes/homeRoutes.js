76574626129724eb330203e37396c39fd301723f
Unknown <danila.vpopov@gmail.com>
2022-03-24 20:09:46 -0700

    fixed handlebars

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,18 +1,18 @@
-const router = require('express').Router();
-const { User } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
 
 // TODO: Add a comment describing the functionality of the withAuth middleware
-router.get('/', withAuth, async (req, res) => {
+router.get("/login", withAuth, async (req, res) => {
   try {
     const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
+      attributes: { exclude: ["password"] },
+      order: [["name", "ASC"]],
     });
 
     const users = userData.map((project) => project.get({ plain: true }));
 
-    res.render('homepage', {
+    res.render("homepage", {
       users,
       // TODO: Add a comment describing the functionality of this property
       logged_in: req.session.logged_in,
@@ -22,14 +22,14 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
-router.get('/login', (req, res) => {
+router.get("/*", (req, res) => {
   // TODO: Add a comment describing the functionality of this if statement
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
 
-  res.render('login');
+  res.render("body");
 });
 
 module.exports = router;
