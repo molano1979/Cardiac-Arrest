5934dd211e61f1c76f8b83a94202a945fb86246f
Unknown <danila.vpopov@gmail.com>
2022-03-26 10:23:26 -0700

    login script and models

--- public/assets/scripts/login.js
+++ public/assets/scripts/login.js
@@ -3,25 +3,25 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // TODO: Add a comment describing the functionality of these expressions
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const userName = document.querySelector("#userName-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
 
-  if (email && password) {
+  if (userName && password) {
     // TODO: Add a comment describing the functionality of this expression
-    const response = await fetch('/api/users/login', {
-      method: 'POST',
-      body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ userName, password }),
+      headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace("/");
     } else {
-      alert('Failed to log in');
+      alert("Failed to log in");
     }
   }
 };
 
 document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
