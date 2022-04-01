0adf082efcbde04089e79e73ba213685eea4b6c5
Unknown <danila.vpopov@gmail.com>
2022-03-31 21:44:31 -0700

    new config

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const helpers = require("./utils/helpers");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+const CONCURRENCY = process.env.WEB_CONCURRENCY || 2;
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
