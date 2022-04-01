c2467b99d2c9935a8fcc3cc93c1d90cd3034cc96
Unknown <danila.vpopov@gmail.com>
2022-03-31 17:26:09 -0700

    Merge branch 'main' of https://github.com/molano1979/Cardiac-Arrest

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, async (req, res) => {
+  try {
+    res.render("homepage", {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    console.log(`${err}`);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+  }
+
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+  }
+
+  res.render("signup");
+});
+
+module.exports = router;
